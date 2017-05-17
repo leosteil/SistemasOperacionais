5cee5ba568c1acc7c6b38bc67fe20edac632bf86
Leonardo <lsteil@inf.ufsm.br>
2017-05-17 15:18:05 -0300

    Sequencial funcionando e inicio com threads

--- t2-SO/lsteil-t2SO.c
+++ t2-SO/lsteil-t2SO.c
@@ -12,6 +12,13 @@ threads/processos (M) que serão utilizados para o processamento.*/
 #include <sys/wait.h>
 #include <errno.h>
 #include <math.h>
+#include <pthreads.h>
+
+
+typedef struct parametrosThreads{
+	int numThread = 0;
+	int *vet;
+}Parametros;
 
 /*função usada para preencher vetor com número aleatórios*/
 /*o range vai até 500*/
@@ -22,10 +29,69 @@ int* preencheVetor(int tam_vetor, int *vetor){
 	srand(time(NULL));
 	for (int i = 0; i < tam_vetor; i++){
 		vetor[i] = rand() % 500;
+		printf("%d ", vetor[i]);
 	}
-
+	printf("\n");
 	return vetor;
 }
+
+int verificaPrimo(int valor, int div){
+	int teste = 1;
+
+	if(valor % div == 0){
+		teste =  0;
+	}
+
+	if(teste == 0){
+		return 0;
+	}else return 1;
+}
+
+void buscaSequencial(int tam_vetor, int *vetor){
+	/*se teste = 0 número não primo, else if teste = 1 número primo*/
+
+	int div = 0;
+	int teste, limite, primos =0;
+
+	for (int i = 0; i < tam_vetor; i++) {
+  		if(vetor[i] > 1){
+  			teste = 1;
+  			div = 2;
+  			limite = sqrt(vetor[i]);;
+  			while(teste == 1 && div < limite){
+  				if((vetor[i] % div) == 0){
+  					teste = 0;
+  				}
+  				div ++;
+  			}
+  			if(teste){
+  				primos ++;
+  			}
+  		}
+  	}
+
+  	printf("Sequencial -> %d\n", primos);
+}
+
+void buscaProcessos(int tam_vetor, int *vetor, int qntdProcessos){
+
+}
+
+void buscaThreads(int tam_vetor, int *vetor, int qntdThreads, pid_t pai){
+	pthreads threads[qntdThreads];
+	Parametros p[qntdThreads];
+
+	for(int i = 0; i < qntdThreads; i++){
+		p[i].numThread = i;
+		p.[i].vetor = vetor;
+		pthread_create(&threads[i], NULL, percorreTrecho, (void*) &args[i]);
+	}
+
+	for (int i = 0; i < NUM_THREADS; i++)
+		pthread_join(threads[i], NULL);
+}
+
+void percorreTrecho()
 
 
 int main(int argc, char *argv[]){
@@ -42,4 +108,6 @@ int main(int argc, char *argv[]){
 
     int *vetor;
    	vetor = preencheVetor(atoi(argv[1]),vetor);
+
+   	buscaSequencial(atoi(argv[1]),vetor);
 }
